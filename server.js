1a1873d674d09e1e271fb5ad5b842721f86ad2f9
Tanvi <tanvi@Tanvis-MacBook-Air.local>
2025-05-16 11:44:06 +1000

    cleanup.

--- server.js
+++ server.js
@@ -4,19 +4,15 @@ const mysql = require('mysql2');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Replace these with your actual Cloud SQL instance credentials
 const dbConfig = {
-  host: '35.202.223.51',         // e.g. '34.168.1.23'
-  DB_USER: 'shubham',
-  DB_PASS: 'Shubham@2020',
-  DB_NAME: 'democloud',
-  INSTANCE_UNIX_SOCKET: '/cloudsql/digital-aloe-455622-i6:us-central1:democloud',
-  INSTANCE_CONNECTION_NAME:'democloud' 
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  socketPath: process.env.INSTANCE_UNIX_SOCKET
 };
-// Create a connection to the Cloud SQL database
+
 const connection = mysql.createConnection(dbConfig);
 
-// Connect to the database
 connection.connect(err => {
   if (err) {
     console.error('Database connection failed:', err.stack);
@@ -25,7 +21,6 @@ connection.connect(err => {
   console.log('Connected to Cloud SQL MySQL database.');
 });
 
-// Sample route
 app.get('/', (req, res) => {
   connection.query('SELECT NOW() AS now', (error, results) => {
     if (error) {
